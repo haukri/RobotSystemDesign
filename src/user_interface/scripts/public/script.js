630ede70c3ccecd2590ad1ef639a1c970bf9fdc4
Rasmus <ralan15@student.sdu.dk>
2019-10-16 09:27:43 +0200

    added functionallity to tabs and added timers in table in settings

--- src/user_interface/scripts/public/script.js
+++ src/user_interface/scripts/public/script.js
@@ -10,5 +10,18 @@ $(document).ready(function() {
         $('#availability-progress').progress({
             percent: msg.stats.availability * 100
         });
+        document.getElementById( "availability2").innerHTML = msg.stats.availability;
+        document.getElementById( "performance").innerHTML = msg.stats.performance;
+        document.getElementById( "quality").innerHTML = msg.stats.quality;
+        document.getElementById( "oee").innerHTML = msg.stats.overall_equipment_effectiveness;
+        document.getElementById( "duration").innerHTML = msg.stats.duration.data.secs;
+        document.getElementById( "heldduration").innerHTML = msg.stats.held_duration.data.secs;
+        document.getElementById( "suspendedduration").innerHTML = msg.stats.susp_duration.data.secs;
+        document.getElementById( "idleduration").innerHTML = msg.stats.idle_duration.data.secs;
+        document.getElementById( "executeduration").innerHTML = msg.stats.exe_duration.data.secs;
     });
+    /*socket.on('packml_stats', function(msg){
+        document.getElementById( "availability").innerHTML = msg.stats.availability
+    });*/
+
 });
